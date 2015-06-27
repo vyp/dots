485cb6414518f900ea8ef0296f125301ca79d117
rws <elisp.vim@gmail.com>
2015-06-27 23:53:55 +1000

    feat(emacs): add colorcolumn at 80 characters

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,10 +1,15 @@
+;; Plugins.
 (add-to-list 'load-path "~/etsi/emacs-packages/undo-tree")
 (add-to-list 'load-path "~/etsi/emacs-packages/goto-chg")
 (add-to-list 'load-path "~/etsi/emacs-packages/evil")
+(add-to-list 'load-path "~/etsi/emacs-packages/fill-column-indicator")
 (add-to-list 'custom-theme-load-path "~/etsi/emacs-packages/sunburst")
 
 (setq evil-want-C-u-scroll t)
+
 (require 'evil)
+(require 'fill-column-indicator)
+
 (evil-mode t)
 
 ;; Theme.
@@ -15,11 +20,9 @@
 (set-face-attribute 'vertical-border nil :foreground "#111")
 (set-face-attribute 'fringe nil :background "#111")
 (set-face-attribute 'lazy-highlight nil :foreground "#111")
+(setq fci-rule-color "#222")
 
-;; Basic settings.
-(setq auto-save-default nil)
-(setq create-lockfiles nil)
-
+;; Appearance (apart from theme).
 (set-face-italic-p 'italic nil)
 (mapc
  (lambda (face)
@@ -28,3 +31,13 @@
 
 (global-linum-mode t)
 (global-hl-line-mode t)
+
+(fringe-mode '(1 . 0))
+(setq-default fill-column 80)
+
+(define-globalized-minor-mode my-global-fci-mode fci-mode turn-on-fci-mode)
+(my-global-fci-mode t)
+
+;; Behaviour.
+(setq auto-save-default nil)
+(setq create-lockfiles nil)
