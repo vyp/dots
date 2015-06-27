85ce29db8d01f47aea41366589ca01810ee72031
rws <elisp.vim@gmail.com>
2015-06-27 22:37:07 +1000

    feat(emacs): sunburst theme

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,17 +1,30 @@
 (add-to-list 'load-path "~/etsi/emacs-packages/undo-tree")
 (add-to-list 'load-path "~/etsi/emacs-packages/goto-chg")
 (add-to-list 'load-path "~/etsi/emacs-packages/evil")
+(add-to-list 'custom-theme-load-path "~/etsi/emacs-packages/sunburst")
 
+(setq evil-want-C-u-scroll t)
 (require 'evil)
 (evil-mode t)
 
-; Newbie (basic?) settings.
+;; Theme.
+(load-theme 'sunburst t)
+(custom-set-faces
+ `(linum ((t (:foreground, "#666"))))
+ `(hl-line ((t (:background, "#222")))))
+(set-face-attribute 'vertical-border nil :foreground "#111")
+(set-face-attribute 'fringe nil :background "#111")
+(set-face-attribute 'lazy-highlight nil :foreground "#111")
+
+;; Basic settings.
 (setq auto-save-default nil)
 (setq create-lockfiles nil)
 
-; Does not seem to work...
 (set-face-italic-p 'italic nil)
-(set-face-bold-p 'bold nil)
+(mapc
+ (lambda (face)
+   (set-face-attribute face nil :weight 'normal :underline nil))
+ (face-list))
 
-; Enable line numbers.
 (global-linum-mode t)
+(global-hl-line-mode t)
