166224c3cc056646ac4bc6017d055c5cbc804ab9
rws <elisp.vim@gmail.com>
2015-06-30 20:14:40 +1000

    feat(emacs): show size column in ibuffer in human readable display
    From [1].
    
    [1]: http://emacswiki.org/emacs/IbufferMode#toc12

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,4 +1,3 @@
-;; TODO: Show size column in ibuffer with kB or MB.
 ;; TODO: Autocompletion.
 ;; TODO: Snippets.
 ;; TODO: Magit.
@@ -45,6 +44,7 @@
 (require 'evil)
 (require 'fill-column-indicator)
 (require 'paren)
+(require 'ibuffer)
 
 (evil-mode t)
 
@@ -79,6 +79,27 @@
 
 (setq show-paren-delay 0)
 (show-paren-mode t)
+
+;; Ibuffer.
+;; Use human readable Size column instead of original one.
+(define-ibuffer-column size-h
+  (:name "Size" :inline t)
+  (cond
+   ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
+   ((> (buffer-size) 100000) (format "%7.0fk" (/ (buffer-size) 1000.0)))
+   ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
+   (t (format "%8d" (buffer-size)))))
+
+;; Modify the default ibuffer formats.
+(setq ibuffer-formats
+      '((mark modified read-only " "
+              (name 18 18 :left :elide)
+              " "
+              (size-h 9 -1 :right)
+              "  "
+              (mode 16 16 :left :elide)
+              " "
+              filename-and-process)))
 
 ;; Theme.
 (load-theme 'sunburst t)
