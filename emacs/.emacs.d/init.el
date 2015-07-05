8fd45e7adc0c42cc992b3d4425051c4825ea59b2
rws <elisp.vim@gmail.com>
2015-07-06 04:09:01 +1000

    refactor(emacs): remove unused pdf utilities

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -46,7 +46,6 @@
 (el-get-bundle! fill-column-indicator)
 (el-get-bundle! auto-complete)
 (el-get-bundle! yasnippet)
-; (el-get-bundle funkenblatt/EPDF)
 (el-get-bundle let-alist)
 (el-get-bundle pdf-tools)
 (el-get-bundle neomantic/Emacs-Sunburst-Color-Theme)
@@ -112,17 +111,6 @@
               (mode 16 16 :left :elide)
               " "
               filename-and-process)))
-
-;; Doc-view.
-; (require 'doc-view)
-; (require 'pdf-util)
-; (setq doc-view-resolution 150)
-; (setq doc-view-continuous t)
-
-; (define-key doc-view-mode-map (kbd "j") 'doc-view-next-line-or-next-page)
-; (define-key doc-view-mode-map (kbd "k") 'doc-view-previous-line-or-previous-page)
-; (define-key doc-view-mode-map (kbd "h") 'image-backward-hscroll)
-; (define-key doc-view-mode-map (kbd "l") 'image-forward-hscroll)
 
 ;; PDF Tools.
 (pdf-tools-install)
