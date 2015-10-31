0a3ec7b1a619afedd2cf579a6f3f2f8087b07c7e
rws <elisp.vim@gmail.com>
2015-10-31 20:26:15 +1100

    feat(emacs): missing basic evil mode motion state bindings

--- emacs/.emacs.d/custom-keybindings/evil-custom-keybindings.el
+++ emacs/.emacs.d/custom-keybindings/evil-custom-keybindings.el
@@ -20,6 +20,7 @@ takes a second \\[keyboard-quit] to abort the minibuffer."
   (evil-window-delete))
 
 ;; Escape quits emacs things as a vim user would expect.
+(define-key evil-motion-state-map           [escape] 'keyboard-quit)
 (define-key evil-normal-state-map           [escape] 'keyboard-quit)
 (define-key evil-visual-state-map           [escape] 'keyboard-quit)
 (define-key minibuffer-local-map            [escape] 'my-minibuffer-keyboard-quit)
@@ -30,23 +31,32 @@ takes a second \\[keyboard-quit] to abort the minibuffer."
 (global-set-key [escape] 'evil-exit-emacs-state)
 (evil-define-key 'normal evil-command-window-mode-map [escape] 'my-exit-evil-command-window)
 
+(define-key evil-motion-state-map "\\" 'evil-switch-to-windows-last-buffer)
 (define-key evil-normal-state-map "\\" 'evil-switch-to-windows-last-buffer)
 
+(define-key evil-motion-state-map (kbd "C-h") 'evil-window-left)
 (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+(define-key evil-motion-state-map (kbd "C-j") 'evil-window-down)
 (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+(define-key evil-motion-state-map (kbd "C-k") 'evil-window-up)
 (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+(define-key evil-motion-state-map (kbd "C-l") 'evil-window-right)
 (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
 
+(define-key evil-motion-state-map   "H" 'evil-first-non-blank)
 (define-key evil-normal-state-map   "H" 'evil-first-non-blank)
 (define-key evil-operator-state-map "H" 'evil-jump-item)
 (define-key evil-visual-state-map   "H" 'evil-first-non-blank)
+(define-key evil-motion-state-map   "L" 'evil-end-of-line)
 (define-key evil-normal-state-map   "L" 'evil-end-of-line)
 (define-key evil-operator-state-map "L" 'evil-end-of-line)
 (define-key evil-visual-state-map   "L" 'evil-end-of-line)
+(define-key evil-motion-state-map   "M" 'evil-jump-item)
 (define-key evil-normal-state-map   "M" 'evil-jump-item)
 (define-key evil-operator-state-map "M" 'evil-jump-item)
 (define-key evil-visual-state-map   "M" 'evil-jump-item)
 
+(define-key evil-motion-state-map (vconcat "z" [return]) 'evil-scroll-line-to-top)
 (define-key evil-normal-state-map (vconcat "z" [return]) 'evil-scroll-line-to-top)
 (define-key evil-visual-state-map (vconcat "z" [return]) 'evil-scroll-line-to-top)
 
@@ -59,6 +69,7 @@ takes a second \\[keyboard-quit] to abort the minibuffer."
 (define-key evil-normal-state-map "'" 'evil-ex)
 (define-key evil-visual-state-map "'" 'evil-ex)
 (define-key evil-normal-state-map "gs" 'evil-write)
+(define-key evil-motion-state-map "Y"  'my-evil-yank-to-end-of-line)
 (define-key evil-normal-state-map "Y"  'my-evil-yank-to-end-of-line)
 
 ;; Leader layer.
@@ -67,22 +78,29 @@ takes a second \\[keyboard-quit] to abort the minibuffer."
   (interactive)
   (evil-edit "~/ui/emacs/.emacs.d/init.el"))
 
+;; Required for the following motion state maps that start with space.
+(define-key evil-motion-state-map " " nil)
+
+(define-key evil-motion-state-map (kbd "SPC SPC") 'recenter-top-bottom)
 (define-key evil-normal-state-map (kbd "SPC SPC") 'recenter-top-bottom)
-(define-key evil-normal-state-map (kbd "SPC dl")  'kill-this-buffer)
+(define-key evil-motion-state-map (kbd "SPC ei")  'my-evil-edit-dot-emacs)
 (define-key evil-normal-state-map (kbd "SPC ei")  'my-evil-edit-dot-emacs)
 (define-key evil-normal-state-map (kbd "SPC f")   'fill-paragraph)
 (define-key evil-visual-state-map (kbd "SPC f")   'evil-fill)
+(define-key evil-motion-state-map (kbd "SPC h")   'help)
 (define-key evil-normal-state-map (kbd "SPC h")   'help)
-(define-key evil-normal-state-map (kbd "SPC q")   'evil-command-window-ex)
-(define-key evil-visual-state-map (kbd "SPC q")   'evil-command-window-ex)
-(define-key evil-normal-state-map (kbd "SPC s")   'evil-window-split)
-(define-key evil-normal-state-map (kbd "SPC v")   'evil-window-vsplit)
+(define-key evil-normal-state-map (kbd "SPC ;")   'evil-command-window-ex)
+(define-key evil-visual-state-map (kbd "SPC ;")   'evil-command-window-ex)
+(define-key evil-motion-state-map (kbd "SPC we")  'balance-windows)
 (define-key evil-normal-state-map (kbd "SPC we")  'balance-windows)
+(define-key evil-motion-state-map (kbd "SPC wo")  'delete-other-windows)
 (define-key evil-normal-state-map (kbd "SPC wo")  'delete-other-windows)
+(define-key evil-motion-state-map (kbd "SPC x")   'execute-extended-command)
 (define-key evil-normal-state-map (kbd "SPC x")   'execute-extended-command)
 (define-key evil-visual-state-map (kbd "SPC x")   'execute-extended-command)
 
 ;; Generic special.
+(define-key evil-motion-state-map (kbd "SPC l") 'ibuffer)
 (define-key evil-normal-state-map (kbd "SPC l") 'ibuffer)
 (define-key evil-normal-state-map "gcc"         'evilnc-comment-or-uncomment-lines)
 (define-key evil-visual-state-map "gc"          'comment-or-uncomment-region)
