e97d3cdd36d17de5f1b2bb09c8129d32d5169ca7
xd1le <elisp.vim@gmail.com>
2015-11-21 10:07:06 +1100

    feat(nix): use latest git version of qutebrowser always

--- docs/installation.md
+++ docs/installation.md
@@ -1,1 +1,4 @@
 *work in progress* (but `/boostrap` script is a basic wip skeleton)
+
+actually this whole document and bootstrap script might become obsolete once
+fully switched to NixOS and/or GuixSD!!
--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -3,11 +3,9 @@ with import <nixpkgs> {};
 {
  packageOverrides = pkgs: rec {
    qutebrowser = pkgs.stdenv.lib.overrideDerivation pkgs.qutebrowser (oldAttrs: {
-     src = fetchgit {
-       url = "https://github.com/The-Compiler/qutebrowser.git";
-       rev = "bb807cfa07640360e281c32066c38a63f406f8bd";
-       sha256 = "0abki6xbn9qdgqqakygwcjlmz1k23x7x8h1v980k6zxnm424y65z";
-     };
+     src = builtins.filterSource
+       # TODO: Factor out the source directory into a variable.
+       (path: type: (toString path) != (toString ~/gh/forks/qutebrowser/.git)) ~/gh/forks/qutebrowser;
    });
  };
 }
