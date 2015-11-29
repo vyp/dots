8c7337cd4b32b9e1593663c8d7ce9c5f4426dc95
xd1le <elisp.vim@gmail.com>
2015-11-29 23:50:55 +1100

    feat(nix): add custom rustc package for miserve

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -1,11 +1,13 @@
 with import <nixpkgs> {};
 
 {
- packageOverrides = pkgs: rec {
-   qutebrowser = pkgs.stdenv.lib.overrideDerivation pkgs.qutebrowser (oldAttrs: {
-     src = builtins.filterSource
-       # TODO: Factor out the source directory into a variable.
-       (path: type: (toString path) != (toString ~/sc/store/qutebrowser/.git)) ~/sc/store/qutebrowser;
-   });
- };
+  packageOverrides = pkgs: rec {
+    qutebrowser = pkgs.stdenv.lib.overrideDerivation pkgs.qutebrowser (oldAttrs: {
+      src = builtins.filterSource
+        # TODO: Factor out the source directory into a variable.
+        (path: type: (toString path) != (toString ~/sc/store/qutebrowser/.git)) ~/sc/store/qutebrowser;
+    });
+
+    rustcMiserve = callPackage ../packages/rustcMiserve.nix {};
+  };
 }
