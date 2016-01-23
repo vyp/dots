cdcda9d36f51fff60d99ef0b3ae7325b1088e1ab
xd1le <elisp.vim@gmail.com>
2016-01-24 00:02:48 +1100

    docs(xmonad): approximation of general xmonad goals

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,5 +1,19 @@
 import XMonad
 
+-- TODO: Find the defaultConfig code.
+-- TODO: Next/previous occupied/free/urgent workspace.
+-- TODO: 10 workspaces.
+-- TODO: Go back to previous workspace if win + the number of the current
+-- workspace is pressed. Allows for quick switching/toggling of workspace views.
+-- TODO: Cartesian window navigation/movement.
+-- TODO: Change ratio/height of windows vertically?
+-- TODO: 'Easymotion'-esque window navigation?
+-- TODO: Check/fix keybindings. (Is it possible to have 'hydra-like'
+-- keybindings?)
+-- TODO: Preselect new window placement?
+-- TODO: Theme/colours => window border colour.
+-- TODO: Panel!
+
 main = xmonad defaultConfig
     { terminal    = "urxvtc"
     , modMask     = mod4Mask
