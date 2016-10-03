7cfb699d27ec276059eba676ddb14723fe916e49
xd1le <elisp.vim@gmail.com>
2016-10-03 14:35:17 +1100

    docs(guile/site/pipe): typo

--- guile/site/pipe.scm
+++ guile/site/pipe.scm
@@ -22,7 +22,7 @@
 ;; The first s-expression will be inserted as the last argument of the next
 ;; s-expesssion.
 ;;
-;; Example: (-> 100 (/ 10) (/ 5)) becomes (/ 5 (/ 10 100))
+;; Example: (->> 100 (/ 10) (/ 5)) becomes (/ 5 (/ 10 100))
 
 (define-syntax ->>
   (syntax-rules ()
