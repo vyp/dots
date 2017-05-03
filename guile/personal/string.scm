db521dee6c9075233e0769a61b14394497fdc91e
xd1le <elisp.vim@gmail.com>
2017-05-03 12:50:23 +1000

    refactor(guile/personal/string): rename substring functions in terms of 'from' and 'to' to make it easier to remember which ones to use

--- guile/personal/string.scm
+++ guile/personal/string.scm
@@ -1,13 +1,13 @@
 (define-module (personal string))
 
-(define-public (substring-end str n)
-  (substring str 0 (- (string-length str) n)))
-
-(define-public (substring-length str start end)
-  (substring str start (- (string-length str) end)))
-
 (define-public (substring-from str n)
   (let ((len (string-length str)))
     (if (= len 0)
         ""
         (substring str (- len n) len))))
+
+(define-public (substring-to str n)
+  (substring str 0 (- (string-length str) n)))
+
+(define-public (substring-from-to str start end)
+  (substring str start (- (string-length str) end)))
