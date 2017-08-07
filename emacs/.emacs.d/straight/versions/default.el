b7ec54ef3e9cf5526007d0350e019f185fa50ddf
xd1le <elisp.vim@gmail.com>
2017-08-07 15:25:42 +1000

    feat(emacs): reset to a minimal init.el that uses straight.el instead of quelpa
    Going to slowly add back all the previous functionality but this is just
    a very basic init.el that allows to connect to IRC for now.
    
    The reason I'm using circe over the bundled rcirc or ERC is because
    rcirc to my understanding does not have (irc) logging, whereas ERC of
    course does, but I couldn't figure out how to configure it properly.. 😕

---
+++ emacs/.emacs.d/straight/versions/default.el
@@ -0,0 +1,7 @@
+(("circe" . "47bbcf4268602688f90c08ce64ec93226fc3f894")
+ ("diminish.el" . "d5c61a14e1a5590a65f83c099a5bd42fcadff24d")
+ ("epkgs" . "a805077922499ab878d8b9b8f6b60a92935918cd")
+ ("melpa" . "6d9ae1e58a1f214a9b88627a2d3254ce7de50740")
+ ("straight.el" . "5aad7999ab7d3682cf061d718286ae27837639a4")
+ ("use-package" . "7b055494e39efba8b237202b7c97f40aa19e2579"))
+:venus
--- emacs/ox.el
+++ emacs/ox.el
@@ -3,7 +3,7 @@
 (dolist (form (cdr (read
                     (with-temp-buffer
                       (insert-file-contents
-                       (expand-file-name "~/ui/emacs/.emacs.d/init.el"))
+                       (expand-file-name "~/dots/emacs/.emacs.d/init.el"))
                       (format "(progn %s)" (buffer-string))))))
   (when (and (eq 'use-package (car form))
              (eq 'org (cadr form)))
