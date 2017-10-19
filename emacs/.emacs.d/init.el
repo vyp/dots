4c319e81c2bd9d09408f7e730b9e3f1c186cc617
xd1le <elisp.vim@gmail.com>
2017-10-19 16:16:32 +1100

    🎁⚙ emacs/evil: set some standard personal keybindings
    (Coming from vim.)

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -121,14 +121,63 @@ urgency flag."
 
 (use-package evil
   :demand t
+  :preface
+  (defun my/evil-yank-to-end-of-line ()
+    (interactive)
+    (evil-yank (point) (point-at-eol)))
+
+  (defun my/insert-two-spaces ()
+    (interactive)
+    (insert "  "))
+
   :init
   (setq evil-cross-lines         t
         evil-shift-width         2
         evil-split-window-below  t
         evil-vsplit-window-right t
-        evil-want-C-u-scroll     t)
+        evil-want-C-u-scroll     t
+        my/leader                "SPC")
+
   :config
-  (evil-mode t))
+  ;; Move up one line when doing `q:` and `q/`.
+  (advice-add 'evil-command-window-ex :after #'evil-previous-line)
+  (advice-add 'evil-command-window-search-forward :after #'evil-previous-line)
+
+  (evil-mode t)
+
+  :general
+  ;; TODO: Make "ESC" quit minibuffer completion stuff?
+
+  ('insert
+   "S-SPC" 'my/insert-two-spaces)
+
+  ('motion
+   ;; Has to be unset so that Emacs can make prefix bindings with non-prefix
+   ;; keys. Make sure this is the same as `my/leader`.
+   "SPC" nil
+
+   ;; TODO: Figure out how to make this work.
+   ; "z RET" 'evil-scroll-line-to-top
+
+   "\\" 'evil-switch-to-windows-last-buffer
+   "'"  'evil-ex
+   "+"  'text-scale-increase
+   "-"  'text-scale-decrease
+   "H"  'evil-first-non-blank
+   "L"  'evil-end-of-line
+   "M"  'evil-jump-item
+   "Y"  'my/evil-yank-to-end-of-line)
+
+  ('motion
+   :prefix my/leader
+   "SPC" 'execute-extended-command
+   "l"   'buffer-menu)
+
+  ('normal
+   "gs" 'evil-write
+   ;; For some reason needs to be in normal state map as well, despite already
+   ;; being in motion state map.
+   "Y"  'my/evil-yank-to-end-of-line))
 
 (use-package files
   :defer t
