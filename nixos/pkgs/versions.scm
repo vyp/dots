1b257d2397d9ef16f6354a9c31613e1a7e4bbd7b
xd1le <elisp.vim@gmail.com>
2017-12-25 20:54:14 +1100

    🎁🎉💡 nixos/pkgs/versions: init
    A version lockfile to track/check for new updates to repositories for
    maintained packages.
    
    It's generated by bin/upd at https://github.com/vyp/scripts.

---
+++ nixos/pkgs/versions.scm
@@ -0,0 +1,57 @@
+(((url . "https://github.com/Vifon/deer")
+  (tag . "v1.4")
+  (date . "2017-08-05")
+  (commit
+    .
+    "4a1be123887633c567334fae5deec3731817e5c0"))
+ ((url .
+       "https://github.com/octaviopardo/EBGaramond12")
+  (tag . #f)
+  (date . "2017-10-26")
+  (commit
+    .
+    "b177f03ecabcfb156d9076f226bdefe2f4e1044b"))
+ ((url . "https://github.com/wingo/fibers")
+  (tag . "v1.0.0")
+  (date . "2017-02-19")
+  (commit
+    .
+    "b86405a2f8daaee3c5d2fcd1b6cefd8e9543c703"))
+ ((url .
+       "https://gitlab.com/interception/linux/tools.git")
+  (tag . "v0.1.1")
+  (date . "2017-08-22")
+  (commit
+    .
+    "c1b69bf1b000355feb8a59f67919e1253dda28fd"))
+ ((url .
+       "https://gitlab.com/interception/linux/plugins/caps2esc.git")
+  (tag . "v0.1.3")
+  (date . "2017-11-29")
+  (commit
+    .
+    "bb09cd8d9a3f04463df55cb4ba63d2d4920e04a9"))
+ ((url . "https://github.com/caryll/otfcc")
+  (tag . "v0.9.5")
+  (date . "2017-09-28")
+  (commit
+    .
+    "32251fffc9cf929a42964eb669c4bf7636a2889b"))
+ ((url . "https://github.com/ttzhou/setroot")
+  (tag . "v2.0.1")
+  (date . "2017-01-22")
+  (commit
+    .
+    "b4a46070a9213807d876fd5bbf0e4040b01e2722"))
+ ((url . "https://github.com/baskerville/sxhkd")
+  (tag . "0.5.8")
+  (date . "2017-04-02")
+  (commit
+    .
+    "18575135809f99f80dbdefa43a840c05682fe8d8"))
+ ((url . "https://github.com/neeasade/xst")
+  (tag . "v0.7.1")
+  (date . "2017-03-14")
+  (commit
+    .
+    "c4f9aa09db4caed27ac609d93c4841b32986c508")))
