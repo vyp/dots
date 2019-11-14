c59c478ee57a9570e72cd715f1ee0362dfe636ca
xd1le <elisp.vim@gmail.com>
2019-11-14 19:33:33 +1100

    🛠 nixos/testoasterror: unstable-2018-12-03 -> unstable-2019-05-12

--- nixos/pkgs/testoasterror/default.nix
+++ nixos/pkgs/testoasterror/default.nix
@@ -2,13 +2,13 @@ with import <nixpkgs> {};
 
 stdenv.mkDerivation rec {
   pname = "testoasterror";
-  version = "unstable-2018-12-03";
+  version = "unstable-2019-05-12";
 
   src = fetchFromGitHub {
     owner = "cylgom";
     repo = pname;
-    rev = "bea3bb8a2234f819682c384bb08be8fa24ff0f6b";
-    sha256 = "133qzl9hj6k9xhjyah9wcs9465svmrsh8xmx1l4s788vf2sih2x9";
+    rev = "71620b47872b5535f87c908883576d73153a6911";
+    sha256 = "0p0l5yzfs5rcbr92m8p7vdg3qv85x1yz7alizcyqpdvvx7a3hxal";
   };
 
   installPhase = ''
